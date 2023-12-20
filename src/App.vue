2fb3ac8ccfaaf79df2892d31f03e0fb3e50f5063
Purp1e <438518244@qq.com>
2023-12-20 10:31:37 +0800

    add: beian

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="window" ref="root" class="w-full h-full flex flex-col justify-center items-center" >
-    <nav :class="{'opacity-0': !showNav}" id="nav" >
+  <div id="window" ref="root" class="flex flex-col items-center justify-center w-full h-full">
+    <nav :class="{ 'opacity-0': !showNav }" id="nav">
       HLAE中文站
     </nav>
 
@@ -10,8 +10,11 @@
       </keep-alive>
     </router-view>
 
+皖公网安备34012302001231
     <footer>
       <span>Presented by <a href="https://github.com/Purple-CSGO" class="author">Purple-CSGO</a> ©2023</span>
+      <a href="https://beian.mps.gov.cn/#/query/webSearch?code=34012302000653" rel="noreferrer"
+        target="_blank" class="beian"><img src="src/assets/img/beian.png" alt="beian"/>皖公网安备34012302000653</a>
     </footer>
   </div>
 </template>
@@ -24,8 +27,8 @@ const showNav = ref(false);
 let nav: HTMLElement;
 
 const onScroll = throttle(200, () => {
-    showNav.value = nav.offsetTop > 120;
-  }, { noLeading: false, noTrailing: false }
+  showNav.value = nav.offsetTop > 120;
+}, { noLeading: false, noTrailing: false }
 )
 
 onMounted(() => {
@@ -50,20 +53,22 @@ onUnmounted(() => {
 }
 
 nav {
-    @apply sticky top-0 w-full h-20 border-b
-    bg-white bg-opacity-70 backdrop-blur-2xl z-30 transition duration-200 ease-in-out
-    flex justify-center items-center
-    font-bold text-xl tracking-wider text-[#3a3a3a]
-    dark:bg-black dark:bg-opacity-70 dark:text-[#eaeaea] dark:border-[#131211];
+  @apply sticky top-0 w-full h-20 border-b bg-white bg-opacity-70 backdrop-blur-2xl z-30 transition duration-200 ease-in-out flex justify-center items-center font-bold text-xl tracking-wider text-[#3a3a3a] dark:bg-black dark:bg-opacity-70 dark:text-[#eaeaea] dark:border-[#131211];
 }
 
 footer {
-  @apply w-full h-28 mt-32 bg-gray-50 text-[#888] flex flex-col items-center justify-center gap-4 text-center tracking-widest transition
-         dark:text-[#aaa] dark:bg-[#262423];
+  @apply w-full h-32 mt-36 bg-gray-100 text-[#888] flex flex-col items-center justify-center gap-4 text-center tracking-widest transition dark:text-[#aaa] dark:bg-[#262423];
 
   .author {
-    @apply text-[#555] hover:text-[#333] font-bold
-           dark:text-[#eee] dark:hover:text-[#fff];
+    @apply text-[#555] hover:text-[#333] font-bold dark:text-[#eee] dark:hover:text-[#fff];
+  }
+
+  .beian {
+    @apply font-normal text-sm tracking-widest text-[#333336] hover:underline underline-offset-2 flex gap-2;
+
+    img {
+      @apply w-4;
+    }
   }
 }
 </style>
